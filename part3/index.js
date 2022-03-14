0f5c1daa3acaefe7a434a21ae0b3750b7fff246e
mateuszdziuba <mkdziuba@gmail.com>
2022-03-14 23:49:51 +0100

    part2 backend

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 let notes = [
     {
@@ -30,6 +31,8 @@ const requestLogger = (request, response, next) => {
   console.log('---')
   next()
 }
+app.use(cors())
+
 app.use(express.json())
 
 app.use(requestLogger)
@@ -96,6 +99,6 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server is running on port ${PORT}`);
