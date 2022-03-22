1701207f92b46bc63ac0d6e8092f9153a841de5a
mateuszdziuba <mkdziuba@gmail.com>
2022-03-22 23:14:52 +0100

    exercises 3.13-3.14, removed phonebook-backend as submodule

---
+++ exercises/phonebook-backend/index.js
@@ -0,0 +1,105 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const cors = require('cors')
+const Person = require('./models/person')
+
+app.use(cors())
+
+app.use(express.static('build'))
+
+// let persons = [
+//     { 
+//         "id": 1,
+//         "name": "Arto Hellas", 
+//         "number": "040-123456"
+//     },
+//     { 
+//         "id": 2,
+//         "name": "Ada Lovelace", 
+//         "number": "39-44-5323523"
+//     },
+//     { 
+//         "id": 3,
+//         "name": "Dan Abramov", 
+//         "number": "12-43-234345"
+//     },
+//     { 
+//         "id": 4,
+//         "name": "Mary Poppendieck", 
+//         "number": "39-23-6423122"
+//     }
+// ]
+
+app.use(express.json())
+
+morgan.token('type', function (req, res) { return JSON.stringify(req.body) })
+
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type')
+)
+
+app.get('/api/persons', (request, response) => {
+    
+    Person.find({}).then(notes => {
+        response.json(notes)
+    })
+})
+
+app.get('/info', (request, response) => {
+    response.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+        `)
+})
+
+
+app.get('/api/persons/:id', (request, response) => {
+    Person.findById(request.params.id).then(note => {
+        response.json(note)
+    })
+    
+}) 
+
+app.delete('/api/persons/:id', (request, response) => {
+    Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/persons', (request, response) => {
+    
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    // if (Person.exists({ name: body.name })) {
+    //     return response.status(400).json({
+    //         error: 'name must be unique'
+    //     })
+    // } 
+
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    })
+
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
+    
+})
+
+
+
+
+const PORT = process.env.PORT
+app.listen(PORT)
+console.log(`Server is running on port ${PORT}`);
