9b70d2d99c141af1cc58da89879318e4613c2d61
mateuszdziuba <mkdziuba@gmail.com>
2022-04-13 14:44:30 +0200

    After exercises 4.8.-4.12.

---
+++ exercises/blog-list/tests/blogs_api.test.js
@@ -0,0 +1,92 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+test('a specific blog is within the returned blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(r => r.title)
+  expect(titles).toContain('React patterns')
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'PHentaiP',
+    author: 'Mikołaj Tarkowski',
+    url: 'http://phent.ai',
+    likes: 69
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const titles = blogsAtEnd.map(r => r.title)
+  expect(titles).toContain('PHentaiP')
+})
+
+test('identifier property is id', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[0].id).toBeDefined();
+})
+
+test('likes property gets added if missing', async () => {
+  const newBlog = {
+    title: 'Newest techs',
+    author: 'Marek Rozwarty',
+    url: 'http://newesttechs.com',
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd.at(-1).likes).toEqual(0)
+})
+
+test('returns status code 400 if title and url are missing', async () => {
+  const newBlog = {
+    author: "Sid Papaja"
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
