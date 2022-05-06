b014b6ab01ac0088a110f581db3c6ede21506b79
mateuszdziuba <mkdziuba@gmail.com>
2022-05-06 16:32:45 +0200

    fixing repo

---
+++ exercises/phonebook-backend/index.js
@@ -0,0 +1,142 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const cors = require('cors')
+const Person = require('./models/person')
+
+app.use(cors())
+
+app.use(express.static('build'))
+
+// let persons = [
+//     {
+//         "id": 1,
+//         "name": "Arto Hellas",
+//         "number": "040-123456"
+//     },
+//     {
+//         "id": 2,
+//         "name": "Ada Lovelace",
+//         "number": "39-44-5323523"
+//     },
+//     {
+//         "id": 3,
+//         "name": "Dan Abramov",
+//         "number": "12-43-234345"
+//     },
+//     {
+//         "id": 4,
+//         "name": "Mary Poppendieck",
+//         "number": "39-23-6423122"
+//     }
+// ]
+
+app.use(express.json())
+
+morgan.token('type', function (req) { return JSON.stringify(req.body) })
+
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
+
+app.get('/api/persons', (request, response) => {
+
+  Person.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+
+app.get('/info', (request, response) => {
+  response.send(`
+      <p>Phonebook has info for ${Person.length} people</p>
+      <p>${new Date()}</p>
+      `)
+})
+
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+
+  const { name, number } = request.body
+
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, conext: 'query' }
+  )
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  // if (Person.exists({ name: body.name })) {
+  //     return response.status(400).json({
+  //         error: 'name must be unique'
+  //     })
+  // }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+    .catch(error => next(error))
+
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT)
+console.log(`Server is running on port ${PORT}`)
