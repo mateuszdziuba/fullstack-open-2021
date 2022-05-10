d6ce0af16837992b43a5953018aab9b5f40f73b4
mateuszdziuba <mkdziuba@gmail.com>
2022-05-10 14:11:47 +0200

    Added connect to NewNote component (legacy)

--- redux-notes/src/components/NewNote.js
+++ redux-notes/src/components/NewNote.js
@@ -1,15 +1,15 @@
-import { useDispatch } from 'react-redux'
+// import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { createNote } from '../reducers/noteReducer'
-import noteService from '../services/notes'
 
 const NewNote = (props) => {
-  const dispatch = useDispatch()
+  // const dispatch = useDispatch()
 
   const addNote = async (event) => {
     event.preventDefault()
     const content = event.target.note.value
     event.target.note.value = ''
-    dispatch(createNote(content))
+    props.createNote(content)
   }
 
   return (
@@ -20,4 +20,4 @@ const NewNote = (props) => {
   )
 }
 
-export default NewNote
+export default connect(null, { createNote })(NewNote)
