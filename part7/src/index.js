6afaacc312fa03874b0316f9cf2b62f310a43421
mateuszdziuba <mkdziuba@gmail.com>
2022-05-11 02:32:28 +0200

    Styled components rock

--- part7/src/index.js
+++ part7/src/index.js
@@ -1,10 +1,39 @@
 import ReactDOM from 'react-dom/client'
 import { useState } from 'react'
+import styled from 'styled-components'
 
 import {
   BrowserRouter as Router,
   Routes, Route, Link, Navigate, useParams, useNavigate, useMatch
 } from 'react-router-dom'
+
+const Button = styled.button`
+  background: Bisque;
+  font-size: 1rem;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid Chocolate;
+  border-radius: 3px;
+`
+
+const Input = styled.input`
+  margin: 0.25em;
+`
+
+const Page = styled.div`
+  padding: 1em;
+  background: papayawhip;
+`
+const Navigation = styled.div`
+  background: BurlyWood;
+  padding: 1em;
+`
+
+const Footer = styled.div`
+  background: Chocolate;
+  padding: 1em;
+  margin-top: 1em;
+`
 
 const Home = () => (
   <div>
@@ -64,12 +93,12 @@ const Login = (props) => {
       <h2>login</h2>
       <form onSubmit={onSubmit}>
         <div>
-          username: <input />
+          username: <Input />
         </div>
         <div>
-          password: <input type='password' />
+          password: <Input type='password' />
         </div>
-        <button type="submit">login</button>
+        <Button type="submit">login</Button>
       </form>
     </div>
   )
@@ -115,8 +144,8 @@ const App = () => {
   }
 
   return (
-    <div>
-      <div>
+    <Page>
+      <Navigation>
         <Link style={padding} to="/">home</Link>
         <Link style={padding} to="/notes">notes</Link>
         <Link style={padding} to="/users">users</Link>
@@ -124,7 +153,7 @@ const App = () => {
           ? <em>{user} logged in</em>
           : <Link style={padding} to="/login">login</Link>
         }
-      </div>
+      </Navigation>
 
       <Routes>
         <Route path="/notes/:id" element={<Note note={note} />} />
@@ -134,10 +163,10 @@ const App = () => {
         <Route path="/" element={<Home />} />
       </Routes>
 
-      <div>
+      <Footer>
         <i>Note app, Department of Computer Science 2022</i>
-      </div>
-    </div>
+      </Footer>
+    </Page>
   )
 }
 
