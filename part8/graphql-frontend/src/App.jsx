6112ce4cb648f0a4a46f6b4d627b80b9eb8a8036
mateuszdziuba <mkdziuba@gmail.com>
2022-05-16 01:23:53 +0200

    Mutation errors, cache

--- part8/graphql-frontend/src/App.jsx
+++ part8/graphql-frontend/src/App.jsx
@@ -1,24 +1,45 @@
-import { gql, useQuery } from "@apollo/client"
+import { useState } from "react"
+import { useQuery } from "@apollo/client"
+import PersonForm from "./components/PersonForm"
 import Persons from './components/Persons'
-
-const ALL_PERSONS = gql`
-query {
-  allPersons {
-    name
-    phone
-    id
-  }
-}`
+import { ALL_PERSONS } from "./queries"
 
 const App = () => {
-  const result = useQuery(ALL_PERSONS)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const result = useQuery(ALL_PERSONS, {
+    // pollInterval: 2000
+  })
 
   if (result.loading) {
     return <div>loading...</div>
   }
 
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
   return (
-    <Persons persons={result.data.allPersons} />
+    <div>
+      <Notify errorMessage={errorMessage} />
+      <Persons persons={result.data.allPersons} />
+      <PersonForm setError={notify}/>
+    </div>
+  )
+}
+
+const Notify = ({ errorMessage }) => {
+  if  (!errorMessage) {
+    return null
+  }
+
+  return (
+    <div style={{color: 'red'}}>
+      {errorMessage}
+    </div>
   )
 }
 
