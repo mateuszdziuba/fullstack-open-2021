443b1735ce52466c53c347e2a7459b48dca3b365
mateuszdziuba <mkdziuba@gmail.com>
2022-05-21 22:17:38 +0200

    Learning TypeScript 👏

---
+++ part9/my-first-ts-app/calculator.ts
@@ -0,0 +1,40 @@
+type Operation = 'multiply' | 'add' | 'divide';
+
+// const calculator = (a: number, b: number, op: Operation) => {
+//   if (op === 'multiply') {
+//     return a * b;
+//   }
+//   if (op === 'add') {
+//     return a + b;
+//   }
+//   if (op === 'divide') {
+//     if (b === 0) return 'can\'t divide by 0!';
+//     return a / b;
+//   }
+// }
+
+// calculator(1, 2, 'yolo');
+
+const calculator = (a: number, b: number, op: Operation): number => {
+  switch(op) {
+    case 'multiply':
+      return a * b;
+    case 'divide':
+      if (b === 0) throw new Error('Can\'t divide by 0!')
+      return a / b;
+    case 'add':
+      return a + b;
+    default:
+      throw new Error('Operation is not multiply, add, or divide!')
+  }
+}
+
+try {
+  console.log(calculator(1, 5, 'divide'));
+} catch (error: unknown) {
+  let errorMessage = 'Something went wrong.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.error(errorMessage)
+}
