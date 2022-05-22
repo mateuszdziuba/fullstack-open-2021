75340995948a22d7ddd40480391066f8acdbf593
mateuszdziuba <mkdziuba@gmail.com>
2022-05-22 02:08:54 +0200

    Exercises 9.1.-9.3.

---
+++ exercises/calculator/bmiCalculator.ts
@@ -0,0 +1,42 @@
+interface BmiParameters {
+  value1: number;
+  value2: number;
+}
+
+const parseArguments = (args: Array<string>): BmiParameters => {
+  if (args.length < 3) throw new Error('Not enougth arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if(!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers')
+  }
+}
+
+const calculateBmi = (height: number, weight: number): string => {
+  const bmi: number = weight / (height / 100)**2
+
+  if (bmi < 16) return 'Underweight (Severe thinness)'
+  if (bmi < 16.9) return 'Underweight (Moderate thinness)'
+  if (bmi < 18.4) return 'Underweight (Mild thinness)	'
+  if (bmi < 24.9) return 'Normal range'
+  if (bmi < 29.9) return 'Overweight (Pre-obese)	'
+  if (bmi < 34.9) return 'Obese (Class I)'
+  if (bmi < 39.9) return 'Obese (Class II)'
+  return 'Obese (Class III)'
+}
+
+try {
+  const { value1, value2 } = parseArguments(process.argv);
+  console.log(calculateBmi(value1, value2));
+} catch (error: unknown) {
+  let errorMessage = 'Error occured.'
+  if (error instanceof Error) {
+    errorMessage += ` Error: ${error.message}`
+  }
+  console.error(errorMessage)
+}
---
+++ exercises/calculator/exerciseCalculator.ts
@@ -0,0 +1,70 @@
+interface ExerciseValues {
+  value1: number;
+  value2: number[];
+}
+
+const parseArguments2 = (args: Array<string>): ExerciseValues => {
+  if (args.length < 3) throw new Error('Not enough arguments');
+
+  const removedArgs = args.splice(0, 3)
+  const totalNumber = removedArgs[2]
+  
+  if(!isNaN(Number(totalNumber)) && args.every(n => !isNaN(Number(n)))) {
+    return {
+      value1: Number(totalNumber),
+      value2: args.map(n => Number(n))
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
+interface ExerciseResult {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const calculateExercises = (target: number, dailyExerciseHours: number[]): ExerciseResult => {
+
+  const periodLength = dailyExerciseHours.length;
+  const trainingDays = dailyExerciseHours.reduce((prev, curr) => prev + (curr ? 0 : 1), 0)
+  const average = dailyExerciseHours.reduce((prev, curr) => prev + curr, 0) / periodLength
+  let rating = 2; 
+  let ratingDescription = 'not too bad but could be better';
+
+  if (average >= target) {
+    rating = 3;
+    ratingDescription = 'good keep it up';
+  }
+
+  if (average < 0.66 * target) {
+    rating = 1;
+    ratingDescription = 'bad';
+  }
+
+  return {
+    periodLength,
+    trainingDays,
+    success: average >= target,
+    rating,
+    ratingDescription,
+    target,
+    average
+  }
+}
+
+try {
+  const { value1, value2 } = parseArguments2(process.argv);
+  console.log(calculateExercises(value1, value2))
+} catch (error: unknown) {
+  let errorMessage = 'Error occured.'
+  if (error instanceof Error) {
+    errorMessage += ` Error: ${error.message}`
+  }
+  console.error(errorMessage)
+}
